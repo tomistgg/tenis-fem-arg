49f5aa4afe844a49e2cf0df041efb6919b6e639b
tomistgg <sebtegg@gmail.com>
2026-02-20 00:49:21 +0100

    this should make Github Actions work

--- itf_load_weekly.py
+++ itf_load_weekly.py
@@ -21,6 +21,10 @@ def get_week_start_end(today=None):
 
 def get_itf_calendar_for_range(start_date, end_date):
     chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     chrome_options.add_argument("window-size=1920,1080")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
